2aeaad816e2e4a8cb391413017bf8bf043ebefd9
sergiotury <sergio.tescobar@alumnos.upm.es>
2018-02-28 08:17:29 +0100

    Practica terminada 2.5

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ biglog('CORE Quiz', 'green');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-    prompt: colorize("quiz> ", 'blue'),
+    prompt: colorize("quiz > ", 'blue'),
     completer: (line) => {
         const completions = 'h help add show delete edit list test p play credits q quit'.split(' ');
         const hits = completions.filter((c) => c.startsWith(line));
